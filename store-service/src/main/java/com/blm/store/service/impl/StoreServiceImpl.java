79617979f6bc529cb0da012a9aad6855cc965f48
SS11041203 <166865497@qq.com>
2025-08-30 11:48:27 +0800

    修复商家FoodCategory权限认证bug

--- store-service/src/main/java/com/blm/store/service/impl/StoreServiceImpl.java
+++ store-service/src/main/java/com/blm/store/service/impl/StoreServiceImpl.java
@@ -126,7 +126,7 @@ public class StoreServiceImpl extends BaseService implements StoreService {
     }
 
     @Override
-    public void verifyStoreOwner(Long storeId, Long userId) {
+    public void verifyStoreOwner(Long userId, Long storeId) {
         Long storeOwnerId = getStoreOwnerId(storeId);
         if (!storeOwnerId.equals(userId)) {
             throw new CommonException(ExceptionConstant.STORE_UNAUTHORIZED);
